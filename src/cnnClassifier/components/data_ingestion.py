98541c79e35b08535651b87a096687164790423d
Gajju9191 <gajananw9191@gmail.com>
2026-01-01 20:10:05 +0530

    data ingestion added

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -7,6 +7,7 @@ from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import DataIngestionConfig
 
 
+
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
@@ -45,3 +46,4 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+
--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,4 +1,5 @@
 
+
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger

9056708cbec889a816405f9f496762bb2e52095a
Gajju9191 <gajananw9191@gmail.com>
2026-01-09 20:33:25 +0530

    evaluation added

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # chest-d-ct--image-data
+
+
+import dagshub
+dagshub.init(repo_owner='Gajju9191', repo_name='chest-d-ct-image-data', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+
+https://dagshub.com/Gajju9191/chest-d-ct-image-data.mlflow
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/Gajju9191/chest-d-ct-image-data.mlflow
+
+export MLFLOW_TRACKING_USERNAME=Gajju9191
+
+export MLFLOW_TRACKING_PASSWORD=089e1f4ec33ad67cc8541160fe89a199ce77186d
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,12 @@
 
+from pathlib import Path
 import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 
 
@@ -81,3 +83,18 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chest-CT-Scan-data"),
+            mlflow_uri="https://dagshub.com/Gajju9191/chest-d-ct-image-data.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+
+        return eval_config
